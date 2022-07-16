0cc941bc688281b8497845e6de8dee0633e21f2d
Chris Richmond <crichmond1989@gmail.com>
2022-07-16 17:12:55 -0500

    add consumer and producer

---
+++ Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig()
+{
+  AutoOffsetReset = AutoOffsetReset.Earliest,
+  BootstrapServers = "localhost:9092",
+  EnableAutoOffsetStore = false,
+  GroupId = "crichmond",
+  PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky,
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("test");
+
+while (true)
+{
+  var result = consumer.Consume(TimeSpan.FromMilliseconds(100));
+
+  if (result == null)
+  {
+    continue;
+  }
+
+  try
+  {
+    Console.WriteLine($"Consumed: partition {result.Partition.Value}, offset {result.Offset.Value}");
+
+    consumer.StoreOffset(result);
+  }
+  catch (KafkaException ex) when (!ex.Error.IsFatal)
+  {
+    Console.WriteLine("Non-fatal exception: {ex}");
+  }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Text;
+using Confluent.Kafka;
+
+var config = new ProducerConfig()
+{
+  BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var tasks = new List<Task>();
+
+var builder = new StringBuilder();
+
+foreach (var _ in Enumerable.Range(0, 20_000))
+{
+  builder.Append("this is a test value");
+}
+
+var value = builder.ToString();
+
+var init = Enumerable.Range(0, 100_000).ToDictionary(x => x.ToString(), x => 1);
+
+var queue = new ConcurrentDictionary<string, int>(init);
+
+var pending = new ConcurrentDictionary<string, int>();
+
+void SleepThenTryAgain(string key, int attempt)
+{
+  Thread.Sleep((2 ^ attempt) * 100);
+
+  queue!.AddOrUpdate(key, _ => attempt + 1, (_, _) => attempt + 1);
+  pending!.TryRemove(new KeyValuePair<string, int>(key, attempt));
+}
+
+void TryProduce(string key, int attempt)
+{
+  var message = new Message<string, string>()
+  {
+    Key = key,
+    Value = value!,
+  };
+
+  try
+  {
+    pending!.AddOrUpdate(key, _ => attempt, (_, _) => attempt);
+
+    Console.WriteLine($"Produce {key}, attempt {attempt}");
+
+    producer!.Produce("test", message, x =>
+    {
+      if (x.Error.Code == ErrorCode.NoError)
+      {
+        Console.WriteLine($"Delivered: {key}");
+
+        pending.TryRemove(new KeyValuePair<string, int>(key, attempt));
+      }
+      else
+      {
+        Console.WriteLine($"Failed: {key}, attempt {attempt}, error {x.Error}");
+        SleepThenTryAgain(key, attempt);
+      }
+    });
+  }
+  catch (KafkaException ex)
+  {
+    if (ex.Error.IsFatal)
+    {
+      throw;
+    }
+
+    Console.WriteLine($"Failed: {key}, attempt {attempt}, error {ex.Error}");
+    SleepThenTryAgain(key, attempt);
+  }
+}
+
+while (queue.Any() || pending.Any())
+{
+  var item = queue.FirstOrDefault();
+
+  if (item.Key != null)
+  {
+    queue.TryRemove(item);
+
+    TryProduce(item.Key, item.Value);
+  }
+  else
+  {
+    producer.Flush(TimeSpan.FromMilliseconds(100));
+  }
+}
+
+Console.WriteLine("All done");
