e51ec8fa31b4973cfb641a0d5c3597e5a6183507
Chris Richmond <crichmond1989@gmail.com>
2022-07-19 10:34:59 -0500

    added notes

--- Service/Hopper.cs
+++ Service/Hopper.cs
@@ -43,10 +43,21 @@ public class Hopper
   {
     Console.WriteLine($"Staged: {key}");
 
+    // NOTE: Due to concurrency, Decrement/Increment needs to be a locked operation. Under the hood,
+    //       the typical _stagedCount++ syntax does three different operations: READ, ADD, SAVE.
+    //       Another thread could call SAVE inbetween this thread's READ -> ADD, or ADD -> SAVE.
+    //       This would cause a missed increment.
     Interlocked.Increment(ref _stagedCount);
 
+    // NOTE: This operation is similar to TryAdd, but it's handy because we're more focused on
+    //       retrieving the value versus setting the value. It's also nice to not have to worry about
+    //       the `var queue` being null. I've seen this implemented as an extension method
+    //       regardless of concurrency.
     var queue = _priorityQueue.GetOrAdd(attempt, _ => new ConcurrentQueue<string>());
 
+    // NOTE: TryAdd returns false if the key already exists. This could occur in our workflow if one
+    //       of the consumers was assigned to Partition X, but then a partition reassignment occured,
+    //       causing another consumer to be assigned Partition X.
     _onDelivered.TryAdd(key, onDelivered);
     _messages.TryAdd(key, message);
 
@@ -65,13 +76,23 @@ public class Hopper
 
     while (IsActive)
     {
+      // NOTE: This is the most effective backoff strategy. There's no need to add more items to
+      //       the queue if the queue is full.
       while (_lastDelivery < _lastRejection)
       {
+        // NOTE: While we're waiting for the queue to empty, we may as well put priority on
+        //       processing delivery reports.
         _producer.Poll(TimeSpan.FromMilliseconds(100));
       }
 
-      var (attempt, queue) = _priorityQueue.OrderByDescending(x => x.Key).FirstOrDefault(x => x.Value.Count > 0);
+      // NOTE: Prioritize retries, so grab the highest attempt queue that has any items
+      var (attempt, queue) = _priorityQueue.OrderByDescending(x => x.Key).FirstOrDefault(x => x.Value.Any());
 
+      // NOTE: Due to concurrency, it's possible that an entity was processed on another thread.
+      //       If either TryRemove returns false, then the entity was processed, and we only
+      //       need to process at least once.
+      //       The TryDequeue will return false when it is empty. If empty, then we Sleep to avoid
+      //       unnecessary processing while we're waiting for more items.
       if (
         queue?.TryDequeue(out var key) == true &&
         _onDelivered.TryRemove(key, out var onDelivered) &&
@@ -96,6 +117,8 @@ public class Hopper
   {
     Console.WriteLine($"Failed: {key}, attempt {attempt}, {error}");
 
+    // NOTE: This Sleep made more sense before the _lastDelivered/_lastRejected logic.
+    //       It might be superfluous now. I'll experiment with removing it.
     Thread.Sleep((attempt ^ 2) * 100);
 
     this.Stage(key, message, onDelivered, attempt + 1);
@@ -123,6 +146,10 @@ public class Hopper
         }
         else
         {
+          // NOTE: SleepThenRetry is run in its own Task to avoid Sleeping an important thread.
+          //       This Task will be removed if I remove the Sleep.
+          //       BTW, I've never seen the flow hit this part of the code. Queue full
+          //       exceptions are thrown on _producer.Produce instead of here.
           Task.Run(() => this.SleepThenRetry(key, message, onDelivered, attempt, x.Error));
         }
       });
